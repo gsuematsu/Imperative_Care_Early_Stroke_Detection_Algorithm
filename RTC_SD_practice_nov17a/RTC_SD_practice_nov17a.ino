58f50e2de1394b9e80670ff1424b99c67f3d39d0
Gregory Suematsu <gsuematsu28@yahoo.com>
2019-11-19 13:05:25 -0800

    Update RTC_SD_practice_nov17a.ino

--- RTC_SD_practice_nov17a/RTC_SD_practice_nov17a.ino
+++ RTC_SD_practice_nov17a/RTC_SD_practice_nov17a.ino
@@ -19,34 +19,35 @@ void setup() {
   }
   Serial.println("initialization done");
 
-//  while(!rtc.begin()) {
-//    Serial.println("Cannot find real time clock");
-//    while(true);
-//  }
-//  while(!rtc.initialized()){
-//    Serial.println("RTC is NOT running");
-//    // This line sets the RTC with an explicit date & time, for example to set
-//    // January 21, 2014 at 3am you would call:
-//    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
-//  }
+  while(!rtc.begin()) {
+    Serial.println("Cannot find real time clock");
+    while(true);
+  }
+  while(!rtc.initialized()){
+    Serial.println("RTC is NOT running");
+    // This line sets the RTC with an explicit date & time, for example to set
+    // January 21, 2014 at 3am you would call:
+    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+  }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-//  DateTime now = rtc.now();
-//
-//  rtc_date += now.year(); rtc_date += "/";
-//  rtc_date += now.month(); rtc_date += "/";
-//  rtc_date += now.day();
-//
-//  rtc_time += now.hour(); rtc_time += ":";
-//  rtc_time += now.minute(); rtc_time += ":";
-//  rtc_time += now.second();
-//
-//  Serial.print(rtc_date);
-//  Serial.print(" ");
-//  Serial.print(rtc_time);
-//  Serial.println();
+  DateTime now = rtc.now();
+
+  rtc_date += now.year(); rtc_date += "/";
+  rtc_date += now.month(); rtc_date += "/";
+  rtc_date += now.day();
+
+  rtc_time += now.hour(); rtc_time += ":";
+  rtc_time += now.minute(); rtc_time += ":";
+  rtc_time += now.second();
+
+  Serial.print(rtc_date);
+  Serial.print(" ");
+  Serial.print(rtc_time);
+  Serial.println();
+  
   // Writes information to file on SD card
   my_file = SD.open("time.txt", FILE_WRITE);
   if(my_file){
