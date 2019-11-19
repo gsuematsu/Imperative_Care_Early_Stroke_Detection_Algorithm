ef49c2ac3bc66ab3fb733098190351da236e2593
gsuematsu <46438335+gsuematsu@users.noreply.github.com>
2019-11-18 19:32:18 -0800

    Update RTC_SD_practice_nov17a.ino

--- RTC_SD_practice_nov17a/RTC_SD_practice_nov17a.ino
+++ RTC_SD_practice_nov17a/RTC_SD_practice_nov17a.ino
@@ -1,9 +1,26 @@
+#include <SPI.h>
+#include <SD.h>
+
+#define PIN_SD_CS 10
+
+File myFile;
+
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
 
+  while(!SD.begin(PIN_SD_CS)) Serial.println("initialization failed");
+  Serial.println("initialization done");
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  myFile = SD.open("time.txt", FILE_WRITE);
+
+  if(myFile){
+    myFile.println(millis());
+    myFile.close();
+  }
 
 }
