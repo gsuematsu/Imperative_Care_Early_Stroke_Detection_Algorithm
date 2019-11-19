7e32e261b666e339edf1e42bf3d972e5b7f11349
gsuematsu <46438335+gsuematsu@users.noreply.github.com>
2019-11-19 11:15:22 -0800

    Update RTC_SD_practice_nov17a.ino

--- RTC_SD_practice_nov17a/RTC_SD_practice_nov17a.ino
+++ RTC_SD_practice_nov17a/RTC_SD_practice_nov17a.ino
@@ -13,26 +13,40 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
 
-  while(!SD.begin(PIN_SD_CS)) Serial.println("SD card initialization failed - insert SD card");
+  if(!SD.begin(PIN_SD_CS)) { 
+    Serial.println("SD card initialization failed - insert SD card");
+    while(true);
+  }
   Serial.println("initialization done");
 
-  while(!rtc.begin()) Serial.println("Cannot find real time clock");
-  while(!rtc.initialized()){
-    Serial.println("RTC is NOT running");
-    // This line sets the RTC with an explicit date & time, for example to set
-    // January 21, 2014 at 3am you would call:
-    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
-  }
+//  while(!rtc.begin()) {
+//    Serial.println("Cannot find real time clock");
+//    while(true);
+//  }
+//  while(!rtc.initialized()){
+//    Serial.println("RTC is NOT running");
+//    // This line sets the RTC with an explicit date & time, for example to set
+//    // January 21, 2014 at 3am you would call:
+//    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+//  }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  DateTime now = rtc.now();
-
-  rtc_date += now.year(); rtc_date += "/";
-  rtc_date += now.month(); rtc_date += "/";
-  rtc_date += now.day(); rtc_date += " ";
-  Serial.println(rtc_date);
+//  DateTime now = rtc.now();
+//
+//  rtc_date += now.year(); rtc_date += "/";
+//  rtc_date += now.month(); rtc_date += "/";
+//  rtc_date += now.day();
+//
+//  rtc_time += now.hour(); rtc_time += ":";
+//  rtc_time += now.minute(); rtc_time += ":";
+//  rtc_time += now.second();
+//
+//  Serial.print(rtc_date);
+//  Serial.print(" ");
+//  Serial.print(rtc_time);
+//  Serial.println();
   // Writes information to file on SD card
   my_file = SD.open("time.txt", FILE_WRITE);
   if(my_file){
